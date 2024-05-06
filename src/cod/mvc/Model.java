ad24009bc6a69d0e6c5fe864da9e00fd9ec01372
Aye14 <amarinconde@danielcastelao.org>
2024-05-06 18:47:51 +0200

    añadiendo crearCcohe() y getCoche()

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -3,5 +3,19 @@ package cod.mvc;
 import java.util.ArrayList;
 
 public class Model {
-    private ArrayList<Coche> parking;
+    private ArrayList<Coche> parking = new ArrayList<>();
+
+    public static Coche crearCoche(String matricula,String modelo,Integer velocidad){
+        Coche cocheNuevo = new Coche(matricula,modelo,velocidad);
+        parking.add(cocheNuevo);
+        return cocheNuevo;
+    }
+    public static Coche getCoche(String matricula) {
+        for (Coche coche : parking) {
+            if (coche.matricula().equals(matricula)) {
+                return coche;
+            }
+        }
+        return null;
+    }
 }
