3c0bdae1d0027154b0b7feee1ee8bc4fec0650d0
Aye14 <amarinconde@danielcastelao.org>
2024-05-06 18:36:51 +0200

    añadiendo clase coche

---
+++ src/cod/mvc/Coche.java
@@ -0,0 +1,13 @@
+package cod.mvc;
+
+public class Coche {
+    private String matricula;
+    private String modelo;
+    private Integer velocidad;
+
+    public Coche(String matricula, String modelo, Integer velocidad){
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.velocidad= velocidad;
+    }
+}
