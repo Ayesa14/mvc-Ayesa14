7e5c6205679748295a1f53ba85c5509a7fd4108f
Aye14 <amarinconde@danielcastelao.org>
2024-05-28 15:53:49 +0200

    app

--- src.com.cod.mvc/App.java
+++ src.com.cod.mvc/App.java
@@ -1,4 +1,17 @@
+import controller.Controler;
+import model.Model;
+
 public class App {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Model miModel = new Model();
+
+        Controler miController = new Controler(miModel);
+
+        miController.crearCoche("LaFerrari", "SBC 1234");
+        miController.crearCoche("Alpine", "HYU 4567");
+
+        miController.cambiarVelocidad("SBC 1234", 30);
+        miController.cambiarVelocidad("HYU 4567", 150);
+    }
 
 }
