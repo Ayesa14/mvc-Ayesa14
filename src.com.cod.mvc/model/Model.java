5dcf1af6326da967ab49d640d9bb98ede0c68964
Aye14 <amarinconde@danielcastelao.org>
2024-05-28 15:53:34 +0200

    Documentacion

--- src.com.cod.mvc/model/Model.java
+++ src.com.cod.mvc/model/Model.java
@@ -6,12 +6,26 @@ import java.util.ArrayList;
 
 public class Model implements Observable {
 
+    public static ArrayList<Coche> parking = new ArrayList<>();
 
+    public static ArrayList<Observer> observers = new ArrayList<>();
+
+    /**
+     * Crea un coche con los parametros especificados
+     * @param matricula variable para el nuevo objecto
+     * @param modelo variable para el nuevo objecto
+     * @return coche creado
+     */
     public static Coche crearCoche(String matricula,String modelo){
         Coche cocheNuevo = new Coche(matricula,modelo);
         parking.add(cocheNuevo);
         return cocheNuevo;
     }
+    /**
+     * Busca un coche por matricula y lo retorna de haberlo encontrado
+     * @param matricula identificador unico del coche
+     * @return coche encontrado o null en caso de no existir el coche
+     */
     public static Coche getCoche(String matricula) {
         for (Coche coche : parking) {
             if (coche.matricula.equals(matricula)) {
@@ -20,14 +34,23 @@ public class Model implements Observable {
         }
         return null;
     }
-
+    /**
+     * Cambia la velocidad del coche cuya matricula a sido especificada
+     * @param matricula identificador unico del coche
+     * @param velocidad variable por la que cambiar la velocidad
+     * @return nueva velocidad
+     */
     public static Integer cambiarVelocidad(String matricula,Integer velocidad){
         Coche coche = getCoche(matricula);
         coche.velocidad = velocidad;
         notifyObservers(coche);
         return coche.velocidad;
     }
-
+    /**
+     * Retorna la velocidad del coche especificado
+     * @param matricula identificador unico del coche
+     * @return velocidad del coche
+     */
     public static Integer getVelocidad(String matricula){
         Coche coche = getCoche(matricula);
         return coche.velocidad;
--- src.com.cod.mvc/model/Observable.java
+++ src.com.cod.mvc/model/Observable.java
@@ -6,9 +6,6 @@ import java.util.ArrayList;
 
 public interface Observable {
 
-    public static ArrayList<Coche> parking = new ArrayList<>();
-
-    public static ArrayList<Observer> observers = new ArrayList<>();
 
     void addObserver(Observer arg);
 
