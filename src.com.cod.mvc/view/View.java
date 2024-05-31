b4a92847280a9b39e9e64aa227c752ef431a33f2
Ayesa Marin <amarinconde@danielcastelao.org>
2024-05-31 09:50:18 +0200

    modificaciones

--- src.com.cod.mvc/view/View.java
+++ src.com.cod.mvc/view/View.java
@@ -1,5 +1,7 @@
 package view;
 
+import javax.swing.JOptionPane;
+
 public class View {
     /**
      * Muestra la velocidad actual del coche cuya matricula se le haya proporcionado
@@ -9,4 +11,25 @@ public class View {
     public static void muestraVelocidad(String matricula,int velocidad){
         System.out.println("La velocidad del coche con matricula "+matricula+", es "+ velocidad + "Km/h");
     }
+    /**
+     * Metodo que muestra matricula, modelo y velocidad de un coche
+     * @param matricula
+     * @param modelo
+     * @param velocidad
+     * @return true
+     */
+    public static boolean muestraCoche(String matricula, String modelo, Integer velocidad) {
+        JOptionPane.showMessageDialog(null,"Matricula: " + matricula + " Modelo: " + modelo
+                + " Velocidad: " + velocidad);
+        return true;
+    }
+
+    /**
+     * Metodo que recoge la matricula del coche con un JOptionPane
+     * @return String
+     */
+    public static String getMatricula() {
+        return JOptionPane.showInputDialog("Introduce la matricula del coche");
+    }
+
 }
